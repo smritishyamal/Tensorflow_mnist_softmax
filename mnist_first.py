4300f28be3279db7ba10710e1f3eaef2dc9a6f6a
Smriti Shyamal <smritishyamal123@gmail.com>
2017-06-29 01:46:28 -0400

    Update mnist_first.py

--- mnist_first.py
+++ mnist_first.py
@@ -25,7 +25,7 @@ train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
 sess = tf.InteractiveSession()
 tf.global_variables_initializer().run()
 
-for _ in range(1000):
+for _ in range(1050):
     batch_xs, batch_ys = mnist.train.next_batch(100)
     sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
     
